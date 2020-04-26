045d31cb9a0656fd7a3ee5b6afaf84cb633a3e31
qriscilla <pri.whang@gmail.com>
2020-04-25 22:43:32 -0400

    Updates

---
+++ precache-manifest.4e906a8e5c6726f331f830a3648123ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94140f9dd88a65081dc0ab4ffc43722",
+    "url": "/finery/index.html"
+  },
+  {
+    "revision": "55038d326715625b08d1",
+    "url": "/finery/static/css/main.ffdc8217.chunk.css"
+  },
+  {
+    "revision": "5c131e813d0dc8fe7162",
+    "url": "/finery/static/js/2.5044d90c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/finery/static/js/2.5044d90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55038d326715625b08d1",
+    "url": "/finery/static/js/main.2ef538a5.chunk.js"
+  },
+  {
+    "revision": "b0d272b5491f9284896c",
+    "url": "/finery/static/js/runtime-main.81d58245.js"
+  },
+  {
+    "revision": "8653b0fca146b14b18bbc8ac865f3eff",
+    "url": "/finery/static/media/storefront1.8653b0fc.png"
+  },
+  {
+    "revision": "a7404d778aaad34cb9123aba5d2b284b",
+    "url": "/finery/static/media/storefront2.a7404d77.png"
+  },
+  {
+    "revision": "f799143b3ae4fe3cd82b9fd628558b85",
+    "url": "/finery/static/media/twig.f799143b.png"
+  }
+]);
