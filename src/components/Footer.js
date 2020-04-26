e96a670c2e81a197c9398a36c4d5c2b81db79a20
qriscilla <pri.whang@gmail.com>
2020-04-26 19:35:46 -0400

    Added background highlight for category links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,88 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+// import { Grid, Table, TableBody, TableRow } from '@material-ui/core';
+// import { withStyles } from "@material-ui/core/styles";
+// import MuiTableCell from "@material-ui/core/TableCell";
+
+const footer = {
+    fontSize: '11px',
+    fontWeight: 'bold',
+}
+const portfolio = {
+    color: 'black'
+}
+// const ul = {
+//     listStyleType: 'none',
+//     fontSize: '12px',
+//     paddingLeft: '0px',
+//     width: '90%'
+// }
+// const title = {
+//     borderBottom: '2px solid black',
+//     fontWeight: 'bold',
+//     fontSize: '12px'
+// }
+// const TableCell = withStyles({
+//     root: {
+//       borderBottom: "none"
+//     }
+//   })(MuiTableCell);
 
 function Footer() {
     return(
-        <div className='footer'>
-            This website was designed and developed by Priscilla Whang.
-        </div>
+        <Grid item container justify='center' style={footer}>
+            {/* <Table>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <ul style={ul}>
+                                <li style={title}>FIND US ON</li>
+                                <li>Instagram</li>
+                                <li>Pinterest</li>
+                            </ul>
+                        </TableCell>
+                        <TableCell>
+                            <ul style={ul}>
+                                <li style={title}>OUR LOCATION</li>
+                                <li>Center City</li>
+                                <li>Philadelphia</li>
+                            </ul>                                    
+                        </TableCell>
+                        <TableCell>
+                            <ul style={ul}>
+                                <li style={title}>INFORMATION</li>
+                                <li>About Finery</li>
+                                <li>Contact Us</li>
+                            </ul>                                    
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table> */}
+            {/* <Grid item xs={4}>
+                <ul style={ul}>
+                    <li style={title}>FIND US ON</li>
+                    <li>Instagram</li>
+                    <li>Pinterest</li>
+                </ul>
+            </Grid>
+            <Grid item xs={4}>
+                <ul style={ul}>
+                    <li style={title}>OUR LOCATION</li>
+                    <li>Center City</li>
+                    <li>Philadelphia, PA</li>
+                </ul>
+            </Grid>
+            <Grid item xs={4}>
+                <ul style={ul}>
+                    <li style={title}>INFORMATION</li>
+                    <li>About Finery</li>
+                    <li>Contact Us</li>
+                </ul>
+            </Grid> */}
+            <Grid xs={12} item container justify='center'>
+                <p>WEBSITE DESIGNED AND DEVELOPED BY{" "}<a href='https://priwhang.com/' target='_blank' rel="noopener noreferrer" style={portfolio}>PRISCILLA WHANG</a></p>
+            </Grid>
+        </Grid>
     );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,46 +7,83 @@ import Rain from '../../images/Rain.png';
 import Mini from '../../images/Mini.png';
 import { Link } from 'react-router-dom';
 
+// Styling: Home
+const home = {
+    textAlign: 'center'
+}
+
+// Styling: Storefront
+const storefront = {
+    position: 'relative',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat'
+}
+const storefrontImage= {
+    width: '100%'
+}
+const storefrontOverlay = {
+    position: 'absolute',
+    top: '35%',
+    width: '100%',
+    fontSize: '7vw',
+    color: 'rgb(230,230,230)',
+    fontWeight: 'bold'
+}
+
+// Styling: Category
+const categories = {
+    marginTop: '20px'
+}
+const categoryLink = {
+    textDecoration: 'none'
+}
+const categoryImage = {
+    width: '90%'
+}
+const categoryP = {
+    fontSize: '1.9rem',
+    fontWeight: 'bold',
+    marginTop: '-50px',
+    color: 'rgb(230,230,230)'
+}
+const categorySpan = {
+    backgroundColor: 'black'
+}
+
 function Home() {
 
     return(
-        <Grid container style={{textAlign:'center'}}>
+        <Grid container className='home' style={home}>
 
             {/* Storefront */}
-            <Grid item container>
-                <Grid item container style={{position:'relative', backgroundSize: "contain", backgroundRepeat:'no-repeat'}}>
-                    <Grid item xs={12} md={6}>
-                        <img src={Storefront1} alt={Storefront1} style={{width:'100%'}} />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <img src={Storefront2} alt={Storefront2} style={{width:'100%'}} />                
-                    </Grid>
+            <Grid item container style={storefront}>
+                <Grid item xs={12} md={6}>
+                    <img src={Storefront1} alt={Storefront1} style={storefrontImage} />
                 </Grid>
-                <p style={{position:'absolute',top:'35%',width:'100%',fontSize:'7vw',color:'rgb(230,230,230)',fontWeight:'bold'}}>Modern basics for lifewear</p>                
+                <Grid item xs={12} md={6}>
+                    <img src={Storefront2} alt={Storefront2} style={storefrontImage} />                
+                </Grid>
             </Grid>
+            <p style={storefrontOverlay}>Modern basics for lifewear</p>                
 
-            {/* Cards */}
-            <Grid item container style={{marginTop:'20px'}}>
-                {/* <Grid item xs={12} sm={4} style={{backgroundImage:`url(${Turtleneck})`, backgroundSize:'contain'}}>
-                    <p style={{position:'absolute',fontSize:'30px',color:'rgb(230,230,230)',fontWeight:'bold'}}>clothes</p>                
-                </Grid> */}
+            {/* Categories */}
+            <Grid item container style={categories}>
                 <Grid item xs={12} sm={4}>
-                    <Link to='/finery/clothes' style={{textDecoration:'none'}}>
-                        <img src={Turtleneck} alt={Turtleneck} style={{width:'90%'}} />
-                        <p style={{fontSize:'30px',color:'rgb(230,230,230)',fontWeight:'bold',marginTop:'-50px'}}>clothes</p>        
-                    </Link>
-        
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Link to='/finery/shoes' style={{textDecoration:'none'}}>
-                        <img src={Rain} alt={Rain} style={{width:'90%'}} />
-                        <p style={{fontSize:'30px',color:'rgb(230,230,230)',fontWeight:'bold',marginTop:'-50px'}}>shoes</p>           
+                    <Link to='/finery/clothes' style={categoryLink}>
+                        <img src={Turtleneck} alt={Turtleneck} style={categoryImage}/>
+                        <p style={categoryP}><span style={categorySpan}>clothes</span></p>    
                     </Link>
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <Link to='/finery/accessories' style={{textDecoration:'none'}}>
-                        <img src={Mini} alt={Mini} style={{width:'90%'}} />
-                        <p style={{fontSize:'30px',color:'rgb(230,230,230)',fontWeight:'bold',marginTop:'-50px'}}>accessories</p>       
+                    <Link to='/finery/shoes' style={categoryLink}>
+                        <img src={Rain} alt={Rain} style={categoryImage}/>
+                        <p style={categoryP}><span style={categorySpan}>shoes</span></p>           
+                    </Link>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Link to='/finery/accessories' style={categoryLink}>
+                        <img src={Mini} alt={Mini} className='categoryImage' style={categoryImage}/>
+                        <p style={categoryP}><span style={categorySpan}>accessories</span></p>       
                     </Link>
                 </Grid>
             </Grid>
